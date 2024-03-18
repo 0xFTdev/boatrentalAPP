e6e1ce4bedab9b2773d2462b9b998fc35568189f
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-03-18 16:42:16 +0100

    stefan updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AppStateProvider } from "./context.jsx";
-import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppStateProvider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </AppStateProvider>
   </React.StrictMode>
 );
