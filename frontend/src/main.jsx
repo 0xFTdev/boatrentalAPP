3a2eb338e1c8a205228f0c7f2cf65f66ef121583
digitalflowerstudio <floriantappeser@gmail.com>
2024-03-18 15:53:30 +0100

    started frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AppStateProvider } from "./context.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppStateProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </AppStateProvider>
   </React.StrictMode>
 );
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        <h2>Current Bookings: 3</h2>
+      </div>
+      <div>
+        <h2>Available Spaceships: 7</h2>
+      </div>
+      <div>
+        <h2>Total Spaceships: 10</h2>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
