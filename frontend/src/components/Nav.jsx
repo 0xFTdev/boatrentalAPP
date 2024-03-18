c7406ee2b6c775bf069afa0d68f41c653fe3f880
digitalflowerstudio <floriantappeser@gmail.com>
2024-03-18 16:14:54 +0100

    nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <>
+      <nav className="p-6">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Home
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Spaceships
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Bookings
+        </button>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
